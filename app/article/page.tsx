7d60947347c68452e533286cdec099865ca40c73
EneaK9 <eneakuca0@gmail.com>
2023-10-20 00:20:12 +0200

    new update

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,41 +1,43 @@
 import { notFound } from "next/navigation";
 import React from "react";
 import LiveTimestamp from "../LiveTimestamp";
-import { useRouter } from "next/router";
 
-function ArticlePage() {
-  const router = useRouter();
+type Props = {
+  searchParams?: Article;
+};
 
-  if (!router.query) {
+function ArticlePage({ searchParams }: Props) {
+  if (
+    (searchParams && Object.entries(searchParams).length === 0) ||
+    !searchParams
+  ) {
     return notFound();
   }
 
-  const { title, author, source, published_at, description, image } =
-    router.query;
-
-  // Ensure that published_at is a string before passing it to LiveTimestamp
-  const time = typeof published_at === "string" ? published_at : "";
-
+  const article: Article = searchParams;
   return (
     <article>
-      <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-        {image && (
+      <section className=" flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
+        {article.image && (
           <img
-            src={image}
-            alt={title}
+            src={article.image}
+            alt={article.title}
             className="h-52 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
           />
         )}
         <div className="px-10">
-          <h1 className="headerTitle px-0 no-underline pb-2">{title}</h1>
+          <h1 className="headerTitle px-0 no-underline pb-2">
+            {article.title}
+          </h1>
           <div className="flex divide-x-2 space-x-4">
-            <h2 className="font-bold">By: {author}</h2>
-            <h2 className="font-bold pl-4">Source: {source}</h2>
+            <h2 className="font-bold">By: {article.author}</h2>
+            <h2 className="font-bold pl-4">Source: {article.source}</h2>
             <p className="pl-4">
-              <LiveTimestamp time={time} />
+              <LiveTimestamp time={article.published_at} />
+              
             </p>
-          </div>
-          <p className="pt-4">{description}</p>
+          </div>{" "}
+          <p className="pt-4">{article.description}</p>
         </div>
       </section>
     </article>
